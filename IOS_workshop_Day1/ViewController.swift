f2d16f2cf3148ac889422245e6aec5ef12512dd0
wutmaster <wutmaster@hotmail.com>
2017-02-21 15:37:02 +0700

    Initial Commit

---
+++ IOS_workshop_Day1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS_workshop_Day1
+//
+//  Created by Piyawut on 2/21/2560 BE.
+//  Copyright © 2560 Piyawut Thongsang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
