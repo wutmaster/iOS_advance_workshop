58f21a3a21590ea0bd6998ca279fb957a76106ab
wutmaster <wutmaster@hotmail.com>
2017-02-24 10:16:59 +0700

    install pod

--- IOS_workshop_Day1/BlackgroundController.swift
+++ IOS_workshop_Day1/BlackgroundController.swift
@@ -12,16 +12,43 @@ class BlackgroundController: UIViewController{
     var backgroundTask: UIBackgroundTaskIdentifier = UIBackgroundTaskInvalid
     
     func regiserBackgroundTask(){
-        
+        //UIApplication เป็น classที่เป็นตัวแทนทั้ง app โดยเรียกผ่าน .share   โดยส่ง beginBackgroundTask  
+//        เพื่อบอก OS ว่าเรากำลังจะทำงานเบื้องหลังเพิ่ม
         self.backgroundTask = UIApplication.shared.beginBackgroundTask { [weak self] in
             self?.endBackgroundTask()
         }
     }
-    
+    // ตอน จบ background task จะต้องส่งตัวเลข (บัตรคิว)ไปบอก os
     func endBackgroundTask(){
         print("Background Task Ended.")
         UIApplication.shared.endBackgroundTask(backgroundTask)
         backgroundTask = UIBackgroundTaskInvalid
+    }
+    
+    
+    @IBAction func onClick(){
+        
+        self.regiserBackgroundTask()
+        DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async{
+            ()-> Void in
+            for i in 0...100000{
+                switch UIApplication.shared.applicationState{
+                case .active:
+                        print("\(i)")
+                case .background:
+                        print("App is background. Next number = \(i)")
+                        print("Backgroun time remaining= \(UIApplication.shared.backgroundTimeRemaining) seconds")
+                case .inactive:
+                        print("Inactive")
+                        break
+                }
+            }
+            if self.backgroundTask != UIBackgroundTaskInvalid{
+                self.endBackgroundTask()
+            }
+            
+        }
+        
     }
     
 }
